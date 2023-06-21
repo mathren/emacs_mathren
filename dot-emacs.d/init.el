9ce227ddaffd5f0199cfa7d92d64ad29abd49149
math <mathren90@gmail.com>
2023-06-21 12:06:26 -0400

    website management work in progress

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -154,25 +154,6 @@
 	   (file+headline "/tmp/Random_notes.org" "Random throughaway notes")
 	   "* %?\n %T")
 	  ))
-  ;; help with publishing html
-  (setq org-publish-project-alist
-	'(("org-notes"
-	   :base-directory "~/Documents/Website/org/"
-	   :base-extension "org"
-	   :publishing-directory "/tmp/public_html/"
-	   :recursive t
-	   :publishing-function org-html-publish-to-html
-	   :headline-levels 4
-	   :auto-preamble t)
-	  ("org-static"
-	   :base-directory "~/Documents/Website/org/"
-	   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-	   :publishing-directory "/tmp/public_html/"
-	   :recursive t
-	   :publishing-function org-publish-attachment)
-	  ("org-publish" :components ("org-notes" "org-static"))
-	  )
-	)
   )
 
 (use-package org-agenda
