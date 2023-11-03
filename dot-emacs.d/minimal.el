f618076aba74195feca489772df204daf1eacbd5
math <mathren90@gmail.com>
2023-11-03 11:08:42 -0400

    update MESA version

--- dot-emacs.d/minimal.el
+++ dot-emacs.d/minimal.el
@@ -94,10 +94,13 @@
 (add-to-list 'load-path "~/.emacs.d/emacs_tools/mesa-major-mode/")
 (require 'mesa-mode)
 (require 'run-star-extras)
-(setq mesa-default-version "15140")
-(setq mesa-version-mesa-dir "/home/math/Documents/Research/codes/mesa/mesa_15140/mesa15140")
+(setq mesa-default-version "r23.05.1")
+(setq mesa-version-mesa-dir "/home/math/Documents/Research/codes/mesa/mesa_23.05.01/mesa-r23.05.1/")
 (setq mesa-mode-enforce-formatting-default t)
 
+
+(add-to-list 'auto-mode-alist '("/run_star_extras.f90$" . (lambda () (f90-mode) (run-star-extras-minor-mode))))
+(add-to-list 'auto-mode-alist '("/run_binary_extras.f90$" . (lambda () (f90-mode) (run-star-extras-minor-mode))))
 (add-to-list 'auto-mode-alist '("/inlist[^/]*$" . mesa-mode))
 (add-to-list 'auto-mode-alist '("\\.defaults$" . (lambda () (mesa-mode) (f90-mode) (view-mode))))
 (add-to-list 'auto-mode-alist '("\\.inc$" . (lambda () (f90-mode) (view-mode))))
