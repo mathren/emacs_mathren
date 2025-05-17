941eddb656c8d9bfb81782b0fe4e350e294acc1f
mrenzo <mrenzo@arizona.edu>
2025-05-17 11:11:50 -0700

    fzf ivy counsel

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -6,6 +6,24 @@
 
 (require 'use-package)
 
+(use-package fzf
+  :ensure t
+  :bind (("C-c f f" . fzf)               ;; FZF in current directory
+	 ("C-c f r" . fzf-recentf)       ;; FZF recent files
+	 ("C-c f g" . fzf-git-files))   ;; FZF Git tracked files
+  :commands (fzf fzf-directory fzf-git-files fzf-recentf)
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+	fzf/executable "fzf"
+	fzf/git-grep-args "-i --line-number %s"
+	;; command used for `fzf-grep-*` functions
+	;; example usage for ripgrep:
+	;; fzf/grep-command "rg --no-heading -nH"
+	fzf/grep-command "grep -nrH"
+	;; If nil, the fzf buffer will appear at the top of the window
+	fzf/position-bottom t
+	fzf/window-height 15))
+
 (use-package all-the-icons)
 
 (use-package no-littering)
@@ -25,53 +43,66 @@
 (company-tng-configure-default)        ;; tab cycles through suggestions
 
 (use-package ivy
- :diminish
- :bind (("C-s" . swiper)
-	;; :map ivy-minibuffer-map
-	;; ("TAB" . ivy-alt-done)
-	;; ("C-l" . ivy-alt-done)
-	;; ("C-j" . ivy-next-line)
-	;; ("C-k" . ivy-previous-line)
-	;; :map ivy-switch-buffer-map
-	;; ("C-k" . ivy-previous-line)
-	;; ("C-l" . ivy-done)
-	;; ("C-d" . ivy-switch-buffer-kill)
-	;; :map ivy-reverse-i-search-map
-	;; ("C-k" . ivy-previous-line)
-	;; ("C-d" . ivy-reverse-i-search-kill)
-	)
- :config
- (ivy-mode 1))
+   :ensure t
+   :diminish
+   :init (ivy-mode 1)
+   :bind (("C-s" . swiper)
+	  ;; :map ivy-minibuffer-map
+	  ;; ("TAB" . ivy-alt-done)
+	  ;; ("C-l" . ivy-alt-done)
+	  ;; ("C-j" . ivy-next-line)
+	  ;; ("C-k" . ivy-previous-line)
+	  ;; :map ivy-switch-buffer-map
+	  ;; ("C-k" . ivy-previous-line)
+	  ;; ("C-l" . ivy-done)
+	  ;; ("C-d" . ivy-switch-buffer-kill)
+	  ;; :map ivy-reverse-i-search-map
+	  ;; ("C-k" . ivy-previous-line)
+	  ;; ("C-d" . ivy-reverse-i-search-kill)
+	  )
 
- (use-package ivy-rich
-   :init
-   (ivy-rich-mode 1)
-   ;; :config
-   ;; (setq ivy-format-function #'ivy-format-function-line)
-   ;; (setq ivy-rich--display-transformers-list
-   ;; 	(plist-put ivy-rich--display-transformers-list
-   ;; 		   'ivy-switch-buffer
-   ;; 		   '(:columns
-   ;; 		     ((ivy-rich-candidate (:width 40))
-   ;; 		      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
-   ;; 		      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
-   ;; 		      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
-   ;; 		      ; return file path relative to project root or `default-directory' if project is nil
-   ;; 		      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
-   ;; 		     :predicate
-   ;; 		     (lambda (cand)
-   ;; 		       (if-let ((buffer (get-buffer cand)))
-   ;; 			   ;; Don't mess with EXWM buffers
-   ;; 			   (with-current-buffer buffer
-   ;; 			     (not (derived-mode-p 'exwm-mode))))))))
    )
+
+   (use-package ivy-rich
+     :init
+     (ivy-rich-mode 1)
+     ;; :config
+     ;; (setq ivy-format-function #'ivy-format-function-line)
+     ;; (setq ivy-rich--display-transformers-list
+     ;; 	(plist-put ivy-rich--display-transformers-list
+     ;; 		   'ivy-switch-buffer
+     ;; 		   '(:columns
+     ;; 		     ((ivy-rich-candidate (:width 40))
+     ;; 		      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
+     ;; 		      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
+     ;; 		      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
+     ;; 		      ; return file path relative to project root or `default-directory' if project is nil
+     ;; 		      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+     ;; 		     :predicate
+     ;; 		     (lambda (cand)
+     ;; 		       (if-let ((buffer (get-buffer cand)))
+     ;; 			   ;; Don't mess with EXWM buffers
+     ;; 			   (with-current-buffer buffer
+     ;; 			     (not (derived-mode-p 'exwm-mode))))))))
+     )
+
+(use-package counsel
+  :ensure t
+  :after ivy
+  :config (counsel-mode 1))
 
 (use-package dired
   :ensure nil
   :commands (dired dired-jump)
   :bind (("C-x C-j" . dired-jump))
-  :custom ((dired-listing-switches "-agho --group-directories-first")))
-(setq global-auto-revert-non-file-buffers t)
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  ;; Enable auto-revert for dired buffers
+  (setq global-auto-revert-non-file-buffers t)
+  (add-hook 'dired-mode-hook 'auto-revert-mode)
+
+  ;; Add FZF integration in dired
+  (define-key dired-mode-map (kbd "C-c C-f") 'fzf))
 
 (use-package nerd-icons-dired
   :hook (dired-mode . nerd-icons-dired-mode))
