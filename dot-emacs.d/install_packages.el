40b2a1d3bc79abdc686cfde9f35e1e4fc8fffcdb
mrenzo <mrenzo@arizona.edu>
2026-04-26 11:32:07 -0700

    cleanup

--- dot-emacs.d/install_packages.el
+++ dot-emacs.d/install_packages.el
@@ -1,23 +1,3 @@
-;; Author: Mathieu Renzo
-;; Keywords: files
-;;
-;; Comment: install some packages. Should be manually run first time
-;;
-;; Copyright (C) 2019-2024 Mathieu Renzo
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see http://www.gnu.org/licenses/.
-
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -26,14 +6,13 @@
 
 (package-initialize)
 
-;; define list of my packages
 (setq package-list '(fzf
 		     nerd-icons
 		     nerd-icons-completion
 		     nerd-icons-ibuffer
 		     nerd-icons-ivy-rich
 		     nerd-icons-dired
-		     ; snakemake-mode
+		     ;; snakemake-mode
 		     auctex
 		     company-box
 		     counsel
@@ -90,7 +69,7 @@
 		     unfill
 		     ))
 
-; install the missing packages
+;; install the missing packages
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
