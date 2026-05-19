30c5081061d7c5bfbfa937848b03b94da6eba11d
mrenzo <mrenzo@arizona.edu>
2026-05-18 23:41:31 -0400

    up MESA

--- dot-emacs.d/minimal.el
+++ dot-emacs.d/minimal.el
@@ -171,8 +171,8 @@
 (add-to-list 'load-path "~/.emacs.d/emacs_tools/mesa-major-mode/")
 (require 'mesa-mode)
 (require 'run-star-extras)
-(setq mesa-default-version "24.08.1")
-(setq mesa-version-mesa-dir "~/Documents/Research/codes/mesa/mesa-24.08.1/")
+(setq mesa-default-version "dev")
+(setq mesa-version-mesa-dir "~/Documents/Research/codes/mesa/mesa-dev/")
 (setq mesa-mode-enforce-formatting-default t)
 
 
